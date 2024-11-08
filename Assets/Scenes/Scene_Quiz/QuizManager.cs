c715591f9a2143a80c44925a5ac7693223bb1c28
hamaboualeg <mouhamedmedamine11@gmail.com>
2024-11-08 19:07:07 +0100

    background quiz and score bug fixed

--- Assets/Scenes/Scene_Quiz/QuizManager.cs
+++ Assets/Scenes/Scene_Quiz/QuizManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
@@ -22,21 +23,45 @@ public class QuizManager : MonoBehaviour
 
     private int currentQuestionIndex;
     private int score;
-    private bool hasAnswered = false; 
+    private bool hasAnswered = false;
+    private bool hasAttempted = false; // Tracks if user has made an attempt
+    private bool isProcessingNextQuestion = false;
     private float shakeThreshold = 1.5f;
     private Vector3 lastAcceleration;
+
+    // Set the desired button color A785CC (RGB: 167, 133, 204)
+    private Color A785CC = new Color(167f / 255f, 133f / 255f, 204f / 255f);
+
+    private Color originalButtonColor;
+    private Color correctAnswerColor = Color.green;
 
     private void Start()
     {
         currentQuestionIndex = 0;
         score = 0;
+
+        // Set the color immediately when the quiz starts
+        if (optionButtons.Length > 0)
+        {
+            originalButtonColor = A785CC; // Set to A785CC color
+            foreach (Button button in optionButtons)
+            {
+                button.GetComponent<Image>().color = originalButtonColor; // Set the color for all buttons initially
+            }
+        }
+
         DisplayQuestion();
         lastAcceleration = Input.acceleration;
+
+        nextButton.onClick.AddListener(OnNextButtonClicked);
     }
 
     private void Update()
     {
-        DetectShake();
+        if (!isProcessingNextQuestion && hasAttempted) // Only detect shake after an attempt
+        {
+            DetectShake();
+        }
     }
 
     private void DetectShake()
@@ -44,7 +69,7 @@ public class QuizManager : MonoBehaviour
         Vector3 currentAcceleration = Input.acceleration;
         if (currentAcceleration.magnitude > shakeThreshold && lastAcceleration.magnitude <= shakeThreshold)
         {
-            NextQuestion();
+            ShowCorrectAnswer();
         }
         lastAcceleration = currentAcceleration;
     }
@@ -53,19 +78,44 @@ public class QuizManager : MonoBehaviour
     {
         if (currentQuestionIndex < questions.Count)
         {
+            Debug.Log($"Displaying question {currentQuestionIndex + 1} of {questions.Count}");
+
             Question currentQuestion = questions[currentQuestionIndex];
             questionText.text = currentQuestion.questionText;
 
+            // Display options and reset button color
             for (int i = 0; i < optionTexts.Length; i++)
             {
-                optionTexts[i].text = currentQuestion.options[i];
-                int index = i;
-                optionButtons[i].onClick.RemoveAllListeners();
-                optionButtons[i].onClick.AddListener(() => CheckAnswer(index));
+                if (i < currentQuestion.options.Length)
+                {
+                    optionTexts[i].text = currentQuestion.options[i];
+                    optionButtons[i].gameObject.SetActive(true);
+                    optionButtons[i].GetComponent<Image>().color = originalButtonColor; // Reset button color to A785CC
+
+                    int index = i;
+                    optionButtons[i].onClick.RemoveAllListeners();
+                    optionButtons[i].onClick.AddListener(() => CheckAnswer(index));
+                }
+                else
+                {
+                    optionButtons[i].gameObject.SetActive(false);
+                }
             }
 
-            hasAnswered = false; 
+            hasAnswered = false;
+            hasAttempted = false; // Reset attempt tracker
             nextButton.gameObject.SetActive(false);
+            isProcessingNextQuestion = false;
+
+            // Update next button text based on question number
+            if (currentQuestionIndex == questions.Count - 1)
+            {
+                nextButton.GetComponentInChildren<TextMeshProUGUI>().text = "Confirm";
+            }
+            else
+            {
+                nextButton.GetComponentInChildren<TextMeshProUGUI>().text = "Next";
+            }
         }
         else
         {
@@ -77,7 +127,8 @@ public class QuizManager : MonoBehaviour
     {
         if (!hasAnswered)
         {
-            hasAnswered = true; 
+            hasAnswered = true;
+            hasAttempted = true; // User has attempted an answer
             if (questions[currentQuestionIndex].correctAnswerIndex == chosenIndex)
             {
                 score++;
@@ -86,22 +137,48 @@ public class QuizManager : MonoBehaviour
         }
     }
 
-    public void NextQuestion()
+    private void OnNextButtonClicked()
     {
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.Count)
+        if (isProcessingNextQuestion) return;
+        isProcessingNextQuestion = true;
+
+        if (currentQuestionIndex == questions.Count - 1)
         {
-            DisplayQuestion();
+            ShowScore();
+            Invoke("LoadNextScene", 2.0f);
         }
         else
         {
-            ShowScore();
+            NextQuestion();
         }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene("Scene plane 1");
+    }
+
+    public void NextQuestion()
+    {
+        currentQuestionIndex++;
+        DisplayQuestion();
     }
 
     private void ShowScore()
     {
+        Debug.Log("All questions completed. Showing score.");
         scoreText.text = "Your score: " + score + "/" + questions.Count;
-        nextButton.gameObject.SetActive(false); 
+        nextButton.gameObject.SetActive(false);
+    }
+
+    private void ShowCorrectAnswer()
+    {
+        if (hasAnswered && hasAttempted) // Only show if the user has attempted an answer
+        {
+            int correctIndex = questions[currentQuestionIndex].correctAnswerIndex;
+            optionButtons[correctIndex].GetComponent<Image>().color = correctAnswerColor;
+
+            Debug.Log("Correct answer shown.");
+        }
     }
 }
